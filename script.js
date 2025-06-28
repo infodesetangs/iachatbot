e85b81086bfd87385527cc2ad51b087f9dd6bb70
infodesetangs <yassinemarelpro@gmail.com>
2025-06-28 14:41:36 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+<script src="https://cdn.jotfor.ms/s/umd/latest/for-embedded-agent.js"></script>
+
+<script>
+
+  window.addEventListener("DOMContentLoaded", function() {
+
+    window.AgentInitializer.init({
+
+      rootId: "JotformAgent-0197b67f34487441a8cdf228e75780add3e3",
+
+      formID: "0197b67f34487441a8cdf228e75780add3e3",
+
+      queryParams: ["skipWelcome=1", "maximizable=1"],
+
+      domain: "https:\/\/eu.jotform.com",
+
+      isInitialOpen: false,
+
+      isDraggable: false,
+
+      background: "linear-gradient(180deg, #6C73A8 0%, #6C73A8 100%)",
+
+      buttonBackgroundColor: "#0066C3",
+
+      buttonIconColor: "#FFFFFF",
+
+      variant: false,
+
+      customizations: {
+
+        greeting: "Yes",
+
+        greetingMessage: "Hi! How can I assist you?",
+
+        pulse: "Yes",
+
+        position: "right"
+
+      }
+
+    });
+
+  });
+
+</script>
